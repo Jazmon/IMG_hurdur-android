5104c456c2a33594731d5df72d671ff03c27a223
Mikko Tossavainen <mikko.tossavainen@hotmail.com>
2016-04-21 00:38:42 +0300

    added upload connection class

--- IMG_hurdur/app/src/main/java/xyz/atte/img_hurdur/BrowseFragment.java
+++ IMG_hurdur/app/src/main/java/xyz/atte/img_hurdur/BrowseFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class BrowseFragment extends Fragment {
+public class    BrowseFragment extends Fragment {
     public BrowseFragment() {
         // Required empty public constructor
     }
